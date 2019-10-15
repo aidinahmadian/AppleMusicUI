6ef97b43882b0c2beaa00d0154a064272ea3b7e6
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2019-10-15 21:02:12 +0330

    Initial Commit

---
+++ AppleMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusic
+//
+//  Created by aidin ahmadian on 10/15/19.
+//  Copyright © 2019 aidin ahmadian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
